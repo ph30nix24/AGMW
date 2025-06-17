0c04211081c054897a863cdf58d9e1f0afb41a7d
Anuj <starlordindia1230@gmail.com>
2025-06-17 15:07:28 +0530

    Mouse follower

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import NewHero from './components/NewHero'
 import Navbar from './components/Navbar'
 import About from './components/About'
@@ -7,9 +7,46 @@ import Story from './components/Story'
 import Contect from './components/Contect'
 import Footer from './components/Footer'
 
+import gsap from 'gsap'
+
+import { useState, useEffect } from 'react';
+
+function useMousePosition() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const updateMousePosition = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', updateMousePosition);
+    return () => window.removeEventListener('mousemove', updateMousePosition);
+  }, []);
+
+  return position;
+}
+
 const App = () => {
+  const movingDiv = useRef();
+  const {x: currentX, y: currentY} = useMousePosition();
+  useEffect(() => {
+    if(movingDiv.current){
+      console.log(currentX, currentY);
+      gsap.to(movingDiv.current, {
+        x: currentX,
+        y: currentY,
+        ease: 'bonus',
+      })
+    }
+    if(!movingDiv.current){
+      console('null');
+    }
+  },[currentX, currentY])
   return (
-    <div className='w-full min-h-screen bg-purple-50 relative overflow-x-hidden no-scrollbar select-none'>
+    <div className='w-full bg-purple-50 relative overflow-x-hidden no-scrollbar select-none'>
+      <div className='w-8 h-8 bg-white fixed top-0 left-0 rounded-full z-20 mix-blend-difference' ref={movingDiv}>
+
+      </div>
       <Navbar/>
       <NewHero/>
       <About/>
